abc5f013e799a65ba1c086426d265d20f59da01b
Saichul Huda <saichulhuda1995@gmail.com>
2025-07-24 01:24:09 +0700

    update gitignore

--- etax_old/modules/penagihan/views/view_daftar_wp.php
+++
@@ -1,7 +0,0 @@
-<table id="wp_bu_table" style=""></table>
-
-<script type="text/javascript">
-	var GLOBAL_VIEW_DAFTAR_WP_VARS = new Array();
-	GLOBAL_VIEW_DAFTAR_WP_VARS["get_list"] = "<?= base_url(); ?>penagihan/kartu_data_wp/get_list_wp";
-</script>
-<script type="text/javascript" src="<?= base_url(); ?>modules/penagihan/scripts/view_daftar_wp.js"></script>
--- etax_old/modules/penetapan/scripts/view_surat_ketetapan.js
+++
@@ -1,8 +0,0 @@
-/**
- * document ready function
- */
-$(document).ready(function(){
-	$("#tbl_menu a").click(function(){
-		load_content($(this).attr('id'));
-	});
-});
